ceefa6f55e9a4322b83c853c763667a315eaef14
Bartek Pacia <barpac02@gmail.com>
2021-11-17 23:48:29 +0100

    fix database-tools formula desc

--- Formula/database-tools.rb
+++ Formula/database-tools.rb
@@ -1,11 +1,11 @@
 class DatabaseTools < Formula
-  desc "Useful program for mobile development."
+  desc "Tools to manage tourist database"
   homepage "https://github.com/bartekpacia/database-tools"
   url "https://github.com/bartekpacia/database-tools.git", :branch => 'master'
   version "0.1.0"
 
   def install
     # TODO
-    bin.install "bin/generate-keystores"
+    bin.install "bin/gh_release"
   end
 end
