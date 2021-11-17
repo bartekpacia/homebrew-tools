1af86a47e972100327d53ebfed73f9903a08ec8a
Bartek Pacia <barpac02@gmail.com>
2021-11-17 23:59:02 +0100

    bump shell-scripts to v0.1.1

--- Formula/shell-scripts.rb
+++ Formula/shell-scripts.rb
@@ -2,7 +2,7 @@ class ShellScripts < Formula
   desc "Bartek's very own collection of shellscripts"
   homepage "https://github.com/bartekpacia/shell-scripts"
   url "https://github.com/bartekpacia/shell-scripts.git", :branch => 'master'
-  version "0.1.0"
+  version "0.1.1"
 
   def install
     bin.install "bin/gh_release"
