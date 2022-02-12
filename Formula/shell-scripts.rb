b8e11c71294a9c99661ea395b4259879522c49d2
Bartek Pacia <barpac02@gmail.com>
2022-02-13 00:21:48 +0100

    add changelog

--- Formula/shell-scripts.rb
+++ Formula/shell-scripts.rb
@@ -2,9 +2,10 @@ class ShellScripts < Formula
   desc "Bartek's very own collection of shellscripts"
   homepage "https://github.com/bartekpacia/shell-scripts"
   url "https://github.com/bartekpacia/shell-scripts.git", :branch => 'master'
-  version "0.1.1"
+  version "0.1.2"
 
   def install
     bin.install "bin/gh_release"
+    bin.install "bin/changelog"
   end
 end
