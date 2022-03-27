d91cc60fa4db734afb4fa76cae7d103d45d27286
Bartek Pacia <barpac02@gmail.com>
2022-03-27 15:28:12 +0200

    add rmtex

--- Formula/shell-scripts.rb
+++ Formula/shell-scripts.rb
@@ -2,10 +2,11 @@ class ShellScripts < Formula
   desc "Bartek's very own collection of shellscripts"
   homepage "https://github.com/bartekpacia/shell-scripts"
   url "https://github.com/bartekpacia/shell-scripts.git", :branch => 'master'
-  version "0.1.2"
+  version "0.1.3"
 
   def install
     bin.install "bin/gh_release"
     bin.install "bin/changelog"
+    bin.install "bin/rmtex"
   end
 end
