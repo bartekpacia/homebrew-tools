079da1d3181e36b025bfacdb225b8aa0abd587c3
Bartek Pacia <barpac02@gmail.com>
2022-04-19 21:48:51 +0200

    add source_env

--- Formula/shell-scripts.rb
+++ Formula/shell-scripts.rb
@@ -2,12 +2,13 @@ class ShellScripts < Formula
   desc "Bartek's very own collection of shellscripts"
   homepage "https://github.com/bartekpacia/shell-scripts"
   url "https://github.com/bartekpacia/shell-scripts.git", :branch => 'master'
-  version "0.1.3"
+  version "0.1.4"
 
   def install
-    bin.install "bin/gh_release"
     bin.install "bin/changelog"
-    bin.install "bin/rmtex"
     bin.install "bin/clsipy"
+    bin.install "bin/gh_release"
+    bin.install "bin/rmtex"
+    bin.install "bin/source_env"
   end
 end
