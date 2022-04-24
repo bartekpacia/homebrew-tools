5cdc4bdd03e91ca045fa699ef804d2d2a8f7ecab
Bartek Pacia <barpac02@gmail.com>
2022-04-24 02:06:33 +0200

    add emu and simu

--- Formula/shell-scripts.rb
+++ Formula/shell-scripts.rb
@@ -2,13 +2,15 @@ class ShellScripts < Formula
   desc "Bartek's very own collection of shellscripts"
   homepage "https://github.com/bartekpacia/shell-scripts"
   url "https://github.com/bartekpacia/shell-scripts.git", :branch => 'master'
-  version "0.1.4"
+  version "0.1.5"
 
   def install
     bin.install "bin/changelog"
     bin.install "bin/clsipy"
+    bin.install "bin/emu"
     bin.install "bin/gh_release"
     bin.install "bin/rmtex"
+    bin.install "bin/simu"
     bin.install "bin/source_env"
   end
 end
