086e39c17c947a1ced307d8b5057e9cc5abed664
Bartek Pacia <barpac02@gmail.com>
2022-05-02 22:07:58 +0200

    first take on adding Rewrap

---
+++ Formula/rewrap.rb
@@ -0,0 +1,17 @@
+class Rewrap < Formula
+  desc "Fast and convenient way to keep column width"
+  homepage "https://github.com/stkb/Rewrap"
+  url "https://registry.npmjs.org/rewrap/-/rewrap-0.1.0.tgz"
+  version "0.1.0"
+
+  depends_on "node"
+
+  def install
+      system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+      bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "true"
+  end
+end
