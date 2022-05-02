45932cadc813da43eb227290e5875a9b769f7ebc
Bartek Pacia <barpac02@gmail.com>
2022-05-02 22:19:00 +0200

    rewrap: add sha256 and license

--- Formula/rewrap.rb
+++ Formula/rewrap.rb
@@ -5,6 +5,8 @@ class Rewrap < Formula
   homepage "https://github.com/stkb/Rewrap"
   url "https://registry.npmjs.org/rewrap/-/rewrap-0.1.0.tgz"
   version "0.1.0"
+  sha256 "6012d7f34544b92d0503b0f4d7b40f846df8544b09b7eee17b8f4ecaa31915f1"
+  license "Apache-2.0"
 
   depends_on "node"
 
