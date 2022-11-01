b7fdc31cea08723daf9ec80e98d4d9779cc15ca6
Bartek Pacia <barpac02@gmail.com>
2022-11-01 14:39:17 +0000

    shell-scripts 0.2.1

--- Formula/shell-scripts.rb
+++ Formula/shell-scripts.rb
@@ -1,7 +1,7 @@
 class ShellScripts < Formula
   desc "Bartek's very own collection of shellscripts"
   homepage "https://github.com/bartekpacia/shell-scripts"
-  url "https://github.com/bartekpacia/shell-scripts.git", tag: "0.2.0", revision: "68463ace92faca85be9a1866f4ada6577a76faa2"
+  url "https://github.com/bartekpacia/shell-scripts.git", tag: "v0.2.1", revision: "6181a568d80082d35447835a6ce689421575fc14"
 
   def install
     bin.install "bin/changelog"
