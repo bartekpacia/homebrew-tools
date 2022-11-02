f357bb0d1620113eec1b9a1e467853863cadc5e6
Bartek Pacia <barpac02@gmail.com>
2022-11-02 23:39:25 +0100

    rename shell-scripts -> scripts

---
+++ Formula/scripts.rb
@@ -0,0 +1,16 @@
+class Scripts < Formula
+  desc "Bartek's very own collection of various scripts"
+  homepage "https://github.com/bartekpacia/scripts"
+  url "https://github.com/bartekpacia/scripts.git", tag: "v0.2.1", revision: "6181a568d80082d35447835a6ce689421575fc14"
+
+  def install
+    bin.install "bin/changelog"
+    bin.install "bin/clsipy"
+    bin.install "bin/emu"
+    bin.install "bin/get_ssh_key"
+    bin.install "bin/gh_release"
+    bin.install "bin/rmtex"
+    bin.install "bin/simu"
+    bin.install "bin/srcenv"
+  end
+end
