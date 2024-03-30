8a0b51ed5baa01e8fdcd00c80b745b06558668cf
Bartek Pacia <barpac02@gmail.com>
2024-03-30 17:21:24 +0000

    scripts 0.5.0

--- Formula/scripts.rb
+++ Formula/scripts.rb
@@ -1,7 +1,7 @@
 class Scripts < Formula
   desc "Bartek's very own collection of various scripts"
   homepage "https://github.com/bartekpacia/scripts"
-  url "https://github.com/bartekpacia/scripts.git", tag: "v0.4.9", revision: "a8de90aae81ac40a479ab65b2298412478acb297"
+  url "https://github.com/bartekpacia/scripts.git", tag: "v0.5.0", revision: "a8de90aae81ac40a479ab65b2298412478acb297"
 
   def install
     bin.install "bin/changelog"
